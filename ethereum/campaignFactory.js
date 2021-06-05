6fb176f640c92c67a898a00181b0881f312c3ccf
davelowqx <lowqixiang@gmail.com>
2021-06-05 19:24:40 +0800

    removed extra comment

--- ethereum/campaignFactory.js
+++ ethereum/campaignFactory.js
@@ -2,11 +2,7 @@ import web3 from "./web3";
 import { abi } from "./build/CampaignFactory.json";
 import { address } from "./contractAddress.json";
 
-// need to get instance of campaign factory from somewhere else
-// other than the application, so can just import this file
-// replace latest factory address here
-const instance = new web3.eth.Contract(abi, address); //"0x3E7A94665c3d3Ab305ac673dE40E3fb1e394DC96"
-
-//console.log(instance);
+const instance = new web3.eth.Contract(abi, address);
+//"0x3E7A94665c3d3Ab305ac673dE40E3fb1e394DC96"
 
 export default instance;
