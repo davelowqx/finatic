47c79548982e5d92955a445a4da2c6050f526f7b
davidwang98 <work.davidwang@gmail.com>
2021-06-23 00:52:44 +0800

    sign up, login, list your company

--- pages/companies/new.js
+++ pages/companies/new.js
@@ -1,5 +1,7 @@
 import React from "react";
 import {
+  Image,
+  Header,
   TextArea,
   Form,
   Button,
@@ -54,76 +56,112 @@ export default function CompanyNew() {
 
   return (
     <Layout>
-      <Grid centered>
-        <Grid.Column width={12}>
-          <h1>List your Company!</h1>
-          <Form error={!!values.errorMessage}>
-            <Form.Input
-              label="Name"
-              placeholder="Apple Inc"
-              value={values.name}
-              onChange={(event) =>
-                setValues({
-                  ...values,
-                  name: event.target.value.substring(0, 31),
-                })
-              }
-              error={values.name.length > 30}
-            />
-            <Form.Input
-              label="Symbol"
-              placeholder="AAPL"
-              value={values.symbol}
-              onChange={(event) =>
-                setValues({
-                  ...values,
-                  symbol: event.target.value.substring(0, 6).toUpperCase(),
-                })
-              }
-              error={values.symbol.length > 5}
-            />
-            <Form.Field
-              control={TextArea}
-              label="Desciption"
-              placeholder="Be as specific as you want!!"
-              value={values.description}
-              onChange={(event) =>
-                setValues({
-                  ...values,
-                  description: event.target.value,
-                })
-              }
-            />
-            <Form.Input
-              label="Shares Outstanding"
-              placeholder="1000"
-              value={values.sharesOutstanding}
-              onChange={(event) => {
-                let value = parseInt(event.target.value.substring(0, 13));
-                setValues({
-                  ...values,
-                  sharesOutstanding: isNaN(value) || value <= 0 ? "" : value,
-                });
-              }}
-              error={values.sharesOutstanding > 1000000000000} //<1T
-            />
-            <Button label="Upload Image" icon="upload" onClick={handleUpload} />
-            <Form.Field>
-              <Checkbox label="I agree to the Terms and Conditions" />
-            </Form.Field>
-            <Button
-              loading={values.loading}
-              fluid
-              primary
-              onClick={handleSubmit}
-            >
-              List
-            </Button>
-            <ProgressBar show={values.loading} percent={10} />
-            <Message error header="Oops!" content={values.errorMessage} />
-          </Form>
-        </Grid.Column>
-      </Grid>
+      <br />
+      <div className="login-container cardborder">
+        <Grid centered>
+          <Grid.Row>
+            <Grid.Column textAlign="center">
+              <Image
+                src="https://previews.dropbox.com/p/orig/ABOFglUnB0co3MIZAsQjxdb-UXaxiGNCkfbnYiLHsfTO5tFnl9z79ILZQgLc7gnIAqenGQhMJs0AeqQTJvFNcmAQByHbp_3msax_QA1V16x0kqYQTfdMIucEivw43tZDAQOmeCfmoBzhkvMWf6tOpDQbLYSMDcv2DuAn2Rlfw3zFkrmLjjZo9ISTCfRCFRXu8xaURtqGeC_R5s0XQOxo38OxpeE_6B863zyryZtuoKj0E2XuGvbp0pl_vVTaRHx7c4-pg7ZDTGp2KuA1GDezIU2vxXrSv0m-P0sCCflMUX8hWbLL6VuIARInYr3tJmZBmYiYhMmNdPheG-ydVpHbKkJx/p.svg"
+                size="mini"
+                centered
+              />
+              <Header as="h2">List your Company!</Header>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column width={12}>
+              <Form size="big" error={!!values.errorMessage}>
+                <Form.Input
+                  label="Name"
+                  placeholder="Apple Inc"
+                  value={values.name}
+                  onChange={(event) =>
+                    setValues({
+                      ...values,
+                      name: event.target.value.substring(0, 31),
+                    })
+                  }
+                  error={values.name.length > 30}
+                />
+                <br />
+                <Form.Input
+                  label="Symbol"
+                  placeholder="AAPL"
+                  value={values.symbol}
+                  onChange={(event) =>
+                    setValues({
+                      ...values,
+                      symbol: event.target.value.substring(0, 6).toUpperCase(),
+                    })
+                  }
+                  error={values.symbol.length > 5}
+                />
+                <br />
+                <Form.Field
+                  control={TextArea}
+                  label="Desciption"
+                  placeholder="Be as specific as you want!!"
+                  value={values.description}
+                  onChange={(event) =>
+                    setValues({
+                      ...values,
+                      description: event.target.value,
+                    })
+                  }
+                />
+                <br />
+                <Form.Input
+                  label="Shares Outstanding"
+                  placeholder="1000"
+                  value={values.sharesOutstanding}
+                  onChange={(event) => {
+                    let value = parseInt(event.target.value.substring(0, 13));
+                    setValues({
+                      ...values,
+                      sharesOutstanding:
+                        isNaN(value) || value <= 0 ? "" : value,
+                    });
+                  }}
+                  error={values.sharesOutstanding > 1000000000000} //<1T
+                />
+                <br />
+                <Button
+                  label="Upload Image"
+                  icon="upload"
+                  onClick={handleUpload}
+                />
+                <br />
+                <br />
+                <br />
+                <Form.Field>
+                  <Checkbox label="I agree to the Terms and Conditions" />
+                </Form.Field>
+                <br />
+                <div className="login-button-container">
+                  <div className="signup-buttons">
+                    <Button
+                      size="big"
+                      fluid
+                      color="blue"
+                      href="/signup"
+                      loading={values.loading}
+                      fluid
+                      primary
+                      onClick={handleSubmit}
+                    >
+                      {" "}
+                      List
+                    </Button>
+                  </div>
+                </div>
+                <ProgressBar show={values.loading} percent={10} />
+                <Message error header="Oops!" content={values.errorMessage} />
+              </Form>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </div>
     </Layout>
   );
 }
--- pages/login.js
+++ pages/login.js
@@ -1,5 +1,13 @@
 import React from "react";
-import { Button, Grid, Divider, Message, Form } from "semantic-ui-react";
+import {
+  Image,
+  Header,
+  Button,
+  Grid,
+  Divider,
+  Message,
+  Form,
+} from "semantic-ui-react";
 import { useRouter } from "next/router";
 import Layout from "../components/layout/Layout";
 import Link from "next/link";
@@ -26,57 +34,82 @@ export default function Login() {
   return (
     <Layout>
       <br />
-      <Grid centered columns={1}>
-        <Grid.Row>
-          <Grid.Column textAlign="center">
-            <h3>Welcome Back!</h3>
-            <div>
-              New to fundSME? <Link href="/signup">Signup</Link>
-            </div>
-          </Grid.Column>
-        </Grid.Row>
-        <Grid.Row>
-          <Grid.Column computer={8} mobile={16}>
-            <Button fluid color="blue" onClick={handleGoogle}>
-              LOGIN WITH GOOGLE
-            </Button>
-            <Divider horizontal>Or</Divider>
-            <Form onSubmit={handleSubmit} error={!!values.errorMessage}>
-              <Form.Input
-                label="Email"
-                placeholder="john@doe.com"
-                value={values.email}
-                onChange={(event) =>
-                  setValues({
-                    ...values,
-                    email: event.target.value.substring(0, 31),
-                  })
-                }
+      <div className="login-container cardborder">
+        <Grid centered columns={1}>
+          <Grid.Row>
+            <Grid.Column textAlign="center">
+              <Image
+                src="https://previews.dropbox.com/p/orig/ABOFglUnB0co3MIZAsQjxdb-UXaxiGNCkfbnYiLHsfTO5tFnl9z79ILZQgLc7gnIAqenGQhMJs0AeqQTJvFNcmAQByHbp_3msax_QA1V16x0kqYQTfdMIucEivw43tZDAQOmeCfmoBzhkvMWf6tOpDQbLYSMDcv2DuAn2Rlfw3zFkrmLjjZo9ISTCfRCFRXu8xaURtqGeC_R5s0XQOxo38OxpeE_6B863zyryZtuoKj0E2XuGvbp0pl_vVTaRHx7c4-pg7ZDTGp2KuA1GDezIU2vxXrSv0m-P0sCCflMUX8hWbLL6VuIARInYr3tJmZBmYiYhMmNdPheG-ydVpHbKkJx/p.svg"
+                size="mini"
+                centered
               />
-              <Form.Input
-                label="Password"
-                placeholder="shhhh..."
-                value={values.password}
-                onChange={(event) =>
-                  setValues({
-                    ...values,
-                    password: event.target.value,
-                  })
-                }
-              />
-              <Message error header="Oops!" content={values.errorMessage} />
-              <Button fluid color="red">
-                LOGIN
-              </Button>
-            </Form>
-          </Grid.Column>
-        </Grid.Row>
-        <Grid.Row>
-          <Grid.Column textAlign="center">
-            <Link href="/">Forgot Password</Link>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
+              <Header as="h3">
+                Welcome to fundSME!
+                <br />
+                <br />
+                Login below or create an account to continue.
+              </Header>
+            </Grid.Column>
+          </Grid.Row>
+          <Grid.Row>
+            <Grid.Column computer={12} mobile={16}>
+              <Form
+                size="big"
+                onSubmit={handleSubmit}
+                error={!!values.errorMessage}
+              >
+                <Form.Input
+                  label="Email"
+                  placeholder="john@doe.com"
+                  value={values.email}
+                  onChange={(event) =>
+                    setValues({
+                      ...values,
+                      email: event.target.value.substring(0, 31),
+                    })
+                  }
+                />
+                <br /> <br />
+                <Form.Input
+                  label="Password"
+                  placeholder="shhhh..."
+                  value={values.password}
+                  onChange={(event) =>
+                    setValues({
+                      ...values,
+                      password: event.target.value,
+                    })
+                  }
+                />
+                <div style={{ marginTop: "-10px" }}>
+                  <Link href="/">Forgot Password?</Link>
+                </div>
+                <Message error header="Oops!" content={values.errorMessage} />
+                <br />
+                <br />
+                <div className="login-button-container">
+                  <div className="login-buttons">
+                    <Button size="big" fluid color="red">
+                      LOGIN
+                    </Button>
+                  </div>
+                  <div className="login-buttons">
+                    <Button size="big" fluid color="blue" href="/signup">
+                      SIGN UP
+                    </Button>
+                  </div>
+                </div>
+                <br />
+                <Divider horizontal>Or</Divider>
+                <br />
+                <Button fluid size="big" color="green" onClick={handleGoogle}>
+                  LOGIN WITH GOOGLE
+                </Button>
+              </Form>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </div>
     </Layout>
   );
 }
